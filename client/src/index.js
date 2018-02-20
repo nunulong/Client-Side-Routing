e4ce3b0b117e9e51fd2b6e2f557cb597b700af22
Ting Wang <felix.wang.1026@gmail.com>
2018-02-20 17:47:17 -0500

    cs5-intensive

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
+  <Router>
+    <div>
+      <div>Application running, add your routing</div>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" component={Movie} />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
